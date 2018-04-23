30f7d0bd8dd83c96a00af93d3a5e33e6dd7b27a0
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/DXRichEditControlAPISample/CodeUtils/CodeExampleGroupChildrenSelector.vb
@@ -0,0 +1,20 @@
+﻿Imports Microsoft.VisualBasic
+Imports DevExpress.Xpf.Grid
+Imports System
+Imports System.Collections
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+
+Namespace DXRichEditControlAPISample
+	Public Class CodeExampleGroupChildrenSelector
+		Implements IChildNodesSelector
+		Private Function SelectChildren(ByVal item As Object) As IEnumerable Implements IChildNodesSelector.SelectChildren
+			If TypeOf item Is CodeExampleGroup Then
+				Return (CType(item, CodeExampleGroup)).Examples
+			End If
+			Return Nothing
+		End Function
+	End Class
+End Namespace
